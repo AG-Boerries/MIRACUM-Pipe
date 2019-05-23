0bc08bee11320ebd81819f9f8d4146cef9458130
scheiblr <raphael.scheible@uniklinik-freiburg.de>
2019-05-23 16:04:52 +0200

    script installation of Trimmomatic

--- tools/install.sh
+++ tools/install.sh
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+
+version_trimmomatic=0.39
+
+
+###############
+# Trimmomatic #
+###############
+
+# remove old verion
+rm -rf Trimmomatic
+
+# download new version
+wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-${version_trimmomatic}.zip \
+    -O trimmomatic.zip
+
+# unzip
+unzip -o trimmomatic.zip
+rm -f trimmomatic.zip
+
+# rename folder and file (neglect version information)
+mv Trimmomatic* Trimmomatic
+mv Trimmomatic/trimmomatic-${version_trimmomatic}.jar Trimmomatic/trimmomatic.jar
