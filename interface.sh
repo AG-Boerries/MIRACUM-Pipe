22f81379856106e33c2e050dd10e533a85dd616f
scheiblr <raphael.scheible@uniklinik-freiburg.de>
2019-06-05 16:57:24 +0200

    adding some skeleton

--- interface.sh
+++ interface.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# TODO: create commands with nice params
+#function usage {
+#    echo "usage: diz-setup_git_ssh [-ch]"
+#    echo "  -c      turn on config only"
+#    echo "  -h      show this help screen"
+#    exit 1
+#}
+#
+#config_only=false
+#
+#while getopts uh option
+#do
+#case "${option}"
+#in
+#    c) config_only='';;
+#    h) usage;;
+#esac
+#done
+
 set -x
 while $1
 do
