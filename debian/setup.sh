37920ce8b36f12f444f5a185d88248ae03651ac3
scheiblr <raphael.scheible@uniklinik-freiburg.de>
2019-08-26 11:20:56 +0200

    switched to java8openj9, system cleanup

--- debian/setup.sh
+++ debian/setup.sh
@@ -5,6 +5,16 @@ DIR_SCRIPT=$(
   pwd -P
 )
 
+function install_java8()
+{
+  ## install jdk8
+  apt-get install -y apt-transport-https ca-certificates wget dirmngr gnupg software-properties-common && \
+  wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add - && \
+  add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ && \
+  apt-get update && apt-get install adoptopenjdk-8-openj9 && apt-get remove default-jdk
+}
+
+
 # packages that are required for installation
 apt-get install -y build-essential gcc-multilib libc-dev git-core cmake patch cmake  \
   autoconf wget zip unzip zlib1g-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libssl-dev\
@@ -18,4 +28,7 @@ apt-get install -y build-essential gcc-multilib libc-dev git-core cmake patch cm
   python3 python3-pysam \
   python3-pip && \
   pip3 install shyaml && \
-  apt-get purge -y python3-pip
+  apt-get purge -y python3-pip && \
+  install_java8 && \
+  apt-get -y purge default-jre && \
+  apt-get -y autoremove
