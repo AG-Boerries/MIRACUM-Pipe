ac511447d0c56c50dfd5bd161882c4eff59d2cb5
scheiblr <raphael.scheible@uniklinik-freiburg.de>
2019-08-30 16:08:00 +0200

    texlive

--- debian/setup.sh
+++ debian/setup.sh
@@ -14,6 +14,18 @@ function install_java8()
   apt-get update && apt-get install adoptopenjdk-8-openj9
 }
 
+function install_texlive()
+{
+  apt-get install texlive texlive-lang-german
+  tlmgr option repository ftp://tug.org/historic/systems/texlive/2018/tlnet-final
+  tlmgr install \
+    breakurl \
+    multirow \
+    wrapfig \
+    varwidth \
+    threeparttable \
+    preprint
+}
 # update packages
 apt-get update
 
@@ -26,14 +38,14 @@ apt-get install -y build-essential gcc-multilib libc-dev git-core cmake patch cm
   ant \
   perl-base \
   r-base-core r-recommended \
-  texlive \
   python3 python3-pysam \
   python3-pip \
   libsnappy-java && \
-  pip3 install shyaml && \
-  apt-get purge -y python3-pip && \
   install_java8 && \
   apt-get -y purge  default-jre default-jdk-headless \
                     openjdk-11-jdk openjdk-11-jdk-headless \
                     openjdk-11-jre openjdk-11-jre-headless && \
+  install_texlive && \
+  pip3 install shyaml && \
+  apt-get purge -y python3-pip && \
   apt-get -y autoremove
