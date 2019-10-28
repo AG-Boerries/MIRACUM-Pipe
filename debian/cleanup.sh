ed0b52e6168eb8949e8024d0b4756c4b5d17a3e3
scheiblr <raphael.scheible@uniklinik-freiburg.de>
2019-10-28 13:33:14 +0100

    cleanup

--- debian/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# packages only required for installation
-apt-get remove -y build-essential libc-dev gcc-multilib git-core cmake zlib1g-dev libncurses-dev patch cmake \
- autoconf wget unzip libbz2-dev liblzma-dev libcurl4-openssl-dev libssl1.0-dev
-apt-get autoremove
