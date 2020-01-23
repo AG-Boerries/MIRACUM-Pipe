81560435875b3f8963af372cf099e65f6bb443c7
Patrick Metzger <patrick.metzger@mol-med.uni-freiburg.de>
2020-01-23 11:37:08 +0100

    included MAF files for each mutation status, i.e. somatic, germline, LoH

--- debian/setup.sh
+++ debian/setup.sh
@@ -43,13 +43,13 @@ apt-get update
 
 # packages that are required for installation
 apt-get install -y build-essential gcc-multilib libc-dev git-core cmake patch cmake ca-certificates \
-  autoconf wget zip unzip zlib1g-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libssl-dev \
+  autoconf wget zip unzip zlib1g-dev libbz2-dev liblzma-dev libcurl4-gnutls-dev libssl-dev libmariadbclient-dev\
   libncurses5-dev libxml2-dev \
   gfortran \
   default-jre \
   ant \
   perl-base \
-  r-base-core r-recommended r-cran-latticeextra r-cran-hmisc\
+  r-base-core r-recommended r-cran-latticeextra r-cran-hmisc r-cran-rmysql\
   python3 python3-pysam \
   python3-pip \
   libsnappy-java && \
