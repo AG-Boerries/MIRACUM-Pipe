33ef15b009d2c4ef1e70f6df3dc14489e2cba63e
Patrick Metzger <patrick@micracum-pipe-docker.ibsm.miracum>
2020-05-26 10:59:32 +0200

    R Package lsei no longer directly available. Install now from cran archive.

--- RScripts/install_packages.R
+++ RScripts/install_packages.R
@@ -14,7 +14,16 @@ BiocManager::install(version = "3.8")
 # TODO package install in a more efficient way
 packages <- c("foreach", "doMC", "openxlsx", "circlize", "knitr", "kableExtra", "OmicCircos", "rtracklayer", "org.Hs.eg.db",
               "Homo.sapiens", "RMySQL", "biomaRt", "Rsamtools", "gtrellis", "ComplexHeatmap",
-              "YAPSA", "SomaticSignatures", "VariantAnnotation", "BSgenome.Hsapiens.UCSC.hg19",
+              "VariantAnnotation", "BSgenome.Hsapiens.UCSC.hg19", "devtools",
               "TxDb.Hsapiens.UCSC.hg19.knownGene", "gdata", "stringi", "tidyr", "ensembldb",
               "EnsDb.Hsapiens.v75", "GenomicRanges", "dplyr", "magrittr", "pracma", "getopt", "IRanges", "DNAcopy")
 BiocManager::install(pkgs = packages, update  = TRUE, ask = FALSE)
+
+url <- "https://cran.r-project.org/src/contrib/Archive/lsei/lsei_1.2-0.1.tar.gz"
+#pkgFile <- "lsei_1.2-0.1.tar.gz"
+#download.file(url = url, destfile = pkgFile)
+#install.packages(pkgs=pkgFile, type="source", repos=NULL)
+#unlink(pkgFile)
+
+devtools::install_url(url)
+BiocManager::install(c("YAPSA", "SomaticSignatures"), update  = TRUE, ask = FALSE)
